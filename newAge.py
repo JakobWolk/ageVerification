95090e556d8d2756e3caeed809baa59d33d5cb78
JakobWolk <55606955+JakobWolk@users.noreply.github.com>
2021-10-04 10:11:16 -0400

    Update newAge.py

--- newAge.py
+++ newAge.py
@@ -4,10 +4,10 @@ import time
 import re
 from datetime import datetime
 
-# setting that variable i to  0.
+# Setting that variable i to  0.
 i = 0
 
-# a while loop starts that is infinte, 0 is always less than or equal to 10.
+# A while loop starts that is infinte, 0 is always less than or equal to 10.
 while i <= 10:
 
     # A list named data is declared
@@ -16,26 +16,53 @@ while i <= 10:
     print("\n" + "Please Swipe Your Drivers License")
     # If true
     while True:
-        # take the input and declare it as the line variable
+        # Take the input and declare it as the line variable
         line = input()
-        # if line has data
+        # If line has data
         if line:
-            # append it to each line
+            # Append it to each line
             data.append(line)
-            # if not break the loop
+            # If not break the loop
         else:
             break
-    # join all the data
+    # Join all the data
     DLinfo = '\n'.join(data)
-
+     
+    # Declare text variable as DLinfo
     text = DLinfo
     
+    # Search the input string for the Date of Birth
     found = re.search('DBB(.+?)DBA', text).group(1)
     
+    
+    # Convert output to useable format
     cardDOB = datetime.strptime(found, "%m%d%Y")
     
-    print(cardDOB)
+ 
+    now = datetime.now()
+
+    current_time = now.strftime("%m-%d-%Y")
     
+    
+    # Print the current time
+    print("\n" + current_time)
+
+    # Print the card DOB
+    print(cardDOB)
+
+
+    DB = cardDOB
+    now = datetime.now()
+    
+    if DB <
+
+    print(db < now)
+    
+    then = datetime(1985, 1, 1)
+    now = datetime.now()
+
+    diff = now - then
+    print(diff)
 
 
 
